a98aaca11011d0b058bf2182cd73b8dd620fa882
Kevin Adu-Poku <kevin.adupoku@outlook.com>
2026-01-07 19:28:28 -0500

    Update page.tsx
    Fixed backslash

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -19,13 +19,13 @@ const TikTokIcon = ({ className }: { className?: string }) => (
 
 // --- COMPONENTS ---
 const PaperTexture = () => (
-  <div className=\"pointer-events-none fixed inset-0 z-50 opacity-[0.4] mix-blend-multiply\">
-    <svg className=\"h-full w-full\">
-      <filter id=\"paper\">
-        <feTurbulence type=\"fractalNoise\" baseFrequency=\"0.9\" numOctaves=\"4\" stitchTiles=\"stitch\" />
-        <feColorMatrix type=\"saturate\" values=\"0\" />
+  <div className="pointer-events-none fixed inset-0 z-50 opacity-[0.4] mix-blend-multiply">
+    <svg className="h-full w-full">
+      <filter id="paper">
+        <feTurbulence type="fractalNoise" baseFrequency="0.9" numOctaves="4" stitchTiles="stitch" />
+        <feColorMatrix type="saturate" values="0" />
       </filter>
-      <rect width=\"100%\" height=\"100%\" filter=\"url(#paper)\" />
+      <rect width="100%" height="100%" filter="url(#paper)" />
     </svg>
   </div>
 );
